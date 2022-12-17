169cbd62121ac230ed8bfe7424c85024764f874a
namitha9eshan <108302885+namitha9eshan@users.noreply.github.com>
2022-12-17 19:30:00 +0530

    Queue temp commit

--- src/FuelManagementSystem/Queue.java
+++ src/FuelManagementSystem/Queue.java
@@ -6,28 +6,53 @@ import java.util.ArrayList;
 public class Queue {
 
 	private int Capacity;
-	private Customer customers[]=new Customer[4];
+	private Customer customers[]=new Customer[10];//Has to change
 	private boolean queueFull;
+    private  int front,rear;
+    private int capacity=10;
+
 
 	Ticket ticket=new Ticket();
 
-
-	public void Enqueue(String Element) {
-		//Insert to Queue
+    Queue(int size){
+        front = 0;
+        rear = 0;
+        capacity = size;
+        customers = new Customer[capacity];
+    }
+	public void Enqueue(Customer Element) {
+		//Has to change
+        if (capacity ==rear){
+            System.out.println("Line is full!");
+            return;
+        }
+        else{
+            customers[rear] = Element;
+            rear++;
+        }
+        return;
 	}
 
 	public void Dequeue() {
 		//Remove from queue
+        if (front == rear) {
+            System.out.println("Line is empty");
+            return;
+        } else {
+            for (int i = 0; i < rear - 1; i++) {
+                customers[i] = customers[i + 1];
+            }
+            if (rear < capacity) {
+                customers[rear] = null;
+                rear--;
+            }
+            return;
+        }
 	}
 
-	public Queue(Customer customers[]) {
-		//Set a Queue if queue free
-		Capacity = Capacity;
 
 
-	}
-
-	public boolean getQueueFull(Object Queue) {
+	public static boolean getQueueFull(Object Queue) {
 		//if the queue is full return true
 
 		return true;
